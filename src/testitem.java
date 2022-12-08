971d00c819f05a8722797558590015e96e0697d2
PetrovR <sdngliv@gmail.com>
2022-12-08 20:59:34 +0400

    -

---
+++ src/testitem.java
@@ -0,0 +1,14 @@
+public class testitem {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0 && i % 15 != 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 && i % 15 != 0) {
+                System.out.println("Buzz");
+            } else if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else
+                System.out.println(i);
+        }
+    }
+}
